8e52796243b119213646c3e7f98cf5548d5baf44
bhar75 <bharathi_n@msn.com>
2023-05-21 22:23:16 +0530

    update Sidebar Component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { links, social } from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <div>Sidebar comp</div>;
+  const { isSideBarOpen, closeSideBar } = useGlobalContext();
+
+  return (
+    <aside className={isSideBarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className="sidebar-header">
+        <img className="logo" src={logo} alt="logo image" />
+        <button className="close-btn" onClick={closeSideBar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
